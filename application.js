aa5101c3a50097662600cd2360413a2118555edc
marc desrosiers <mdesrosiers@norauto.fr>
2018-05-01 21:57:55 +0200

    solve problem with carousel and update features

--- application.js
+++ application.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-  $('#video').click(function(){
-    $('#rooms-video').slideToggle('slow');
-    $('#video').html('X');
-    $('#video').addClass('animated shake');
-  });
+  $('.carousel').carousel('pause');
+  console.log('Hello world!')
 });
-
